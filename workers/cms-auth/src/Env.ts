3aa279dc842016716fc4d88433081f3f3ac3349f
Anton G. Neuhold Jr. <agneuhold@gmail.com>
2026-05-23 13:12:00 -0700

    Fix bug (#3)

--- workers/cms-auth/src/Env.ts
+++ workers/cms-auth/src/Env.ts
@@ -1,5 +1,5 @@
 export interface Env {
-  GITHUB_CLIENT_ID: string;
-  GITHUB_CLIENT_SECRET: string;
+  GITHUB_CMS_CLIENT_ID: string;
+  GITHUB_CMS_CLIENT_SECRET: string;
   RATE_LIMITER: RateLimit;
 }
